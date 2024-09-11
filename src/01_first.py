c47f1e705896bced9fdf6c637499ca1e072600ad
Sebastian Perez Perez <230110689@itsoeh.edu.mx>
2024-09-11 08:39:15 -0600

    Cambios
    Signed-off-by: Sebastian Perez Perez <230110689@itsoeh.edu.mx>

--- src/01_first.py
+++ src/01_first.py
@@ -1,25 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 
-# Función para verificar el estado del juego
 def check_winner():
-    # Comprobar filas
+
     for i in range(1, 4):
         if buttons[(i, 1)]["text"] == buttons[(i, 2)]["text"] == buttons[(i, 3)]["text"] != " ":
             return buttons[(i, 1)]["text"]
-        
-    # Comprobar columnas
+
     for i in range(1, 4):
         if buttons[(1, i)]["text"] == buttons[(2, i)]["text"] == buttons[(3, i)]["text"] != " ":
             return buttons[(1, i)]["text"]
 
-    # Comprobar diagonales
     if buttons[(1, 1)]["text"] == buttons[(2, 2)]["text"] == buttons[(3, 3)]["text"] != " ":
         return buttons[(1, 1)]["text"]
     if buttons[(1, 3)]["text"] == buttons[(2, 2)]["text"] == buttons[(3, 1)]["text"] != " ":
         return buttons[(1, 3)]["text"]
 
-    # Comprobar empate
     if all(buttons[pos]["text"] != " " for pos in buttons):
         return "Empate"
 
@@ -66,10 +62,10 @@ buttons = {}
 turn = 1
 
 positions = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
-for pos in positions:
-    button = ttk.Button(mainframe, text=" ", width=5, command=lambda p=pos: on_button_click(p))
-    button.grid(column=pos[0], row=pos[1], sticky=W)
-    buttons[pos] = button
+for lugarb in positions:
+    button = ttk.Button(mainframe, text=" ", width=5, command=lambda p=lugarb: on_button_click(p))
+    button.grid(column=lugarb[0], row=lugarb[1], sticky=W)
+    buttons[lugarb] = button
 
 restart_btn = ttk.Button(mainframe, text="Reiniciar", command=restart_game)
 restart_btn.grid(column=1, row=5, sticky=(W, E))
